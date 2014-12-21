7cef902d11c012e49efa58f514e61c3a457a22c8
wangcp@msgcopy.com <wangcp@msgcopy.com>
2014-12-21 23:12:45 +0800

    article_info.html youhua

--- blog/static/blog/js/article_info.js
+++ blog/static/blog/js/article_info.js
@@ -1,4 +1,15 @@
 $(function(){
-	
-	snow_sdk.ajax();
+	var url = window.location.href,
+		article_id = parseInt(url.substr(url.indexOf("/blog/")+6));
+	snow_sdk.ajax("/iapi/blog/article/"+ article_id +"/","GET","",function(data){
+		var $main = $("#main"),
+			$con = $main.find(".container>.content");
+		$main.find("h3.title").html(data["title"]);
+		$con.empty();
+		if(data["ctype"] == "link"){
+			$con.html($("<iframe src='"+ data["content"] +"'></iframe>"));
+		}else{
+			$con.html(data["content"]);
+		}
+	},function(){});
 });
